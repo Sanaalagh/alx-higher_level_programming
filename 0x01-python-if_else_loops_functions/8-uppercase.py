a175da786a7c04b68d604fd38783ebe7eea2550c
Sanaalagh <Sanaalaghzou@gmail.com>
2023-11-28 13:03:37 -0800

    the if else loops project

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return True
-    else:
-        return False
-
-
 def uppercase(str):
-    for c in str:
-        print("{:c}".format(ord(c) if not islower(c) else ord(c) - 32), end="")
-        print("")
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+    print("{:s}".format(i), end="")
+    print()

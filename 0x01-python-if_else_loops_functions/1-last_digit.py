ac375f49b495fc11d76e1f107ef4643b6c8e5a97
Sanaalagh <Sanaalaghzou@gmail.com>
2023-11-28 11:09:19 -0800

    the if else loops project

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 strng = number % 10 if number > 10 else number % -10
 print(
-        "last digit of {:d} is {:d} and is ".format(number,strng), end="")
+        "last digit of {:d} is {:d} and is".format(number, strng), end="")
 if strng > 5:
-    print("Greater than 5")
+    print("greater than 5")
 elif strng == 0:
     print("0")
 else:
--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,6 +4,3 @@ for i in range (25, -1, -1):
     if i % 2 == 1:
         c += 32
     print("{:c}".format(c), end ="" )
-~                                                                 
-~                                                                 
-~                                     

c767ce5c75555a03b75af258711a3e3b3f8b8c14
Sanaalagh <Sanaalaghzou@gmail.com>
2023-12-04 09:17:04 -0800

    0x03. Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for inmatrix in matrix:
+        if len(inmatrix) == 0:
+            print()
+        for i in range(len(inmatrinx)):
+            print("'{:d}".format(inmatrix[i]),end ="\n" if i is len(inmatrix) - 1 else " ")
---
+++ 0x03-python-data_structures/READMe.md
@@ -0,0 +1,2 @@
+It is a README.md file for the 0x03. Python - Data Structures: Lists, Tuples
+project 

e4b67fb190c571095860c2d796b84a0bd5012a1a
Sanaalagh <Sanaalaghzou@gmail.com>
2023-12-19 16:27:24 -0800

    the 0x06. Python - Classes and Objects project

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,78 +1,110 @@
 #!/usr/bin/python3
-"""creates class Node to define linked list node & class SinglyLinkedList"""
+"""Defines the classes Node and SinglyLinkedList"""
 
 
 class Node:
-    """defines class for singly linked list node"""
+    """
+    Class that defines properties Node.
 
+    Attributes:
+        data: data field of node.
+    """
     def __init__(self, data, next_node=None):
+        """Creates new instances of node.
+
+        Args:
+            __data : data field of node.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """ getter for data attribute"""
-        return(self.__data)
+        """Retrieves the data field instance.
+
+        Returns: the data field of a node.
+        """
+        return self.__data
 
     @data.setter
     def data(self, value):
-        """setter for data attribute"""
-        if type(value) is not int:
+        """Propery setter for data.
+
+        Args:
+            value (int): data field of a node.
+
+        Raises:
+            TypeError: data must be an integer
+        """
+        if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        else:
-            self.__data = value
+        self.__data = value
 
     @property
     def next_node(self):
-        """ getter for next_node attribute"""
-        return(self.__next_node)
+        """Retrives the next_node instance.
+
+        Returns: The next_node instance.
+        """
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """setter for next_node attribute"""
+        """Property setter for Node.
+
+        Args:
+            value (None): next node of a Node.
+
+        Raises:
+            TypeError: next_node must be a Node object .
+        """
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
-        else:
-            self.__next_node = value
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-    """defines class for singly linked list"""
+    """
+    Class that defines properties of SinglyLinkedList.
 
+    Attributes:
+        head: head of the SinglyLinkedList.
+    """
     def __init__(self):
+        """Creates new instances of SinglyLinkedList .
+
+        Args:
+            __head : head of the SinglyLinkedList .
+        """
         self.__head = None
 
+    def __str__(self):
+        """Represents the class objects as a string.
+
+        Returns: The class object represented as a string.
+        """
+        temp_var = self.__head
+        print_node = []
+        while temp_var:
+            print_node.sort()
+            print_node.append(str(temp_var.data))
+            temp_var = temp_var.next_node
+
+        print_node.sort(key=int)
+        return ("\n".join(print_node))
+
     def sorted_insert(self, value):
-        """insert new node"""
-        new_node = Node(value)
+        """Inserts a new node at a given position.
+
+        Args:
+            value: value.
+        """
         if self.__head is None:
+            new_node = Node(value)
+            new_node.next_node = self.__head
             self.__head = new_node
         else:
-            tmp = self.__head
-            if tmp.data > new_node.data:
-                new_node.next_node = tmp
-                self.__head = new_node
-                return
-            while tmp.next_node is not None:
-                tmp2 = tmp.next_node
-                if tmp2.data < new_node.data:
-                    tmp = tmp2
-                else:
-                    new_node.next_node = tmp.next_node
-                    tmp.next_node = new_node
-                    return
-            tmp.next_node = new_node
-
-    def stringrep(self):
-        strrep = ""
-        tmp = self.__head
-        while tmp is not None:
-            datavalue = tmp.data
-            strrep = strrep + str(datavalue)
-            tmp = tmp.next_node
-            if tmp:
-                strrep = strrep + "\n"
-        return strrep
-
-    def __repr__(self):
-        return(self.stringrep())
+            new_node = Node(value)
+            new_node.data = value
+            new_node.next_node = self.__head
+            self.__head = new_node
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,37 +1,58 @@
 #!/usr/bin/python3
-"""creates class Square with
-private instance attribute size and public instance method"""
+"""Defines a class Square"""
 
 
 class Square:
-    """defines class with instantiated and validated private instance attribute
-and public instance method."""
+    """
+    Class that defines properties of square by: (based on 4-square.py).
 
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size=0):
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
+        """
         self.__size = size
+
+    def area(self):
+        """Calculates the area of square.
+
+        Returns: the current square area.
+        """
+        return self.__size ** 2
 
     @property
     def size(self):
-        return(self.__size)
+        """Returns the size of a square
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """calculates and returns current square area"""
-        return(self.__size * self.__size)
+        else:
+            self.__size = value
 
     def my_print(self):
-        """prints square of size self.__size using #"""
-        if self.__size > 0:
-            for column in range(self.__size):
-                for row in range(self.__size):
-                    print("#", end="")
-                print()
-        else:
+        """prints in stdout the square with the character #
+        """
+
+        if self.__size == 0:
             print()
+        for i in range(self.__size):
+            print("#" * (self.__size))

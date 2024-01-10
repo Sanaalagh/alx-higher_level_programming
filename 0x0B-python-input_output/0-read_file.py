de0fb9c0dc39befd93e4030451b8c57ab9a0d0c5
Sanaalagh <Sanaalaghzou@gmail.com>
2024-01-10 10:25:42 -0800

    the project 0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Reads a text file (UTF8) and prints it to stdout
+"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """
+    Args:
+        filename: name of the file to read
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")

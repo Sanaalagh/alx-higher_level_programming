65999b0bbf73a30fe22547c703f240b0fd89d406
Sanaalagh <Sanaalaghzou@gmail.com>
2024-01-10 16:30:58 -0800

    the0x07. Python - Test-driven development project

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,32 +1,49 @@
+#include <Python.h>
 #include <stdio.h>
 #include <string.h>
-#include <Python.h>
+#include <float.h>
+#include <sys/types.h>
+#include <python3.4/Python.h>
+#include <python3.4/object.h>
+#include <python3.4/listobject.h>
+#include <python3.4/floatobject.h>
+#include <python3.4/unicodeobject.h>
+#define PY_SSIZE_T_C
+#define PY_SSIZE_T_CLEAN
 
 /**
- * print_python_string - The function that prints strings in Python
- * @p: A pointer to python object.
- * Return: No return.
+ * print_python_string - The implementation for print python bytes function
+ * @p: Pointer to the Python string
+ * Return: If p is not a valid PyBytesObject, print an error message
  */
+
 void print_python_string(PyObject *p)
 {
-	PyObject *str, *repr;
+	Py_ssize_t str_len;
+	Py_UNICODE *str = NULL;
+	char *fmt_str = "  value: %ls\n";
 
-	(void)repr;
+	fflush(stdout);
 	printf("[.] string object info\n");
-
-	if (strcmp(p->ob_type->tp_name, "str"))
+	fflush(stdout);
+	if ((p != NULL) && (p->ob_type != NULL)
+		&& ((p->ob_type)->tp_name != NULL)
+		&& (strcmp((p->ob_type)->tp_name, "str") == 0))
+	{
+		str = PyUnicode_AsWideCharString(p, &str_len);
+		printf("  type: %s%s\n",
+			   ((PyASCIIObject *)p)->state.compact ? "compact " : "",
+			   ((PyASCIIObject *)p)->state.ascii ? "ascii" : "unicode object");
+		fflush(stdout);
+		printf("  length: %d\n", (int)str_len);
+		fflush(stdout);
+		printf(fmt_str, str);
+		fflush(stdout);
+		PyMem_Free(str);
+	}
+	else
 	{
 		printf("  [ERROR] Invalid String Object\n");
-		return;
+		fflush(stdout);
 	}
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
 }

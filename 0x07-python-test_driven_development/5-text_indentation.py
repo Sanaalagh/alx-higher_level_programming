b67f589a940119e2e2c2809679c5f869414e81fd
Sanaalagh <Sanaalaghzou@gmail.com>
2024-01-10 16:14:25 -0800

    the 0x07. Python - Test-driven development project

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
-"""It defines a function that prints a text with 2 new lines after each,
-of these characters: . ? and :
-Attributes:
-    txt_indentation: a function that prints a text with specific conditions
+"""
+prints text in special format
 """
 
 
-def txt_indentation(text):
-    """It prints a text with 2 new lines after .?: characters.
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters: . ? :
 
     Args:
-        text (str): string to be examined.
+        text (str): a string
 
     Raises:
-        TypeError: If text is not of type str.
+        TypeError: if text is not a string
     """
+    new = ""
+    substring = ""
+    temp = [] * 2
+    flag = 0
 
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    for delim in ".:?":
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)])
-
-    print(text, end="")
+    for i in range(len(text)):
+        if i < len(text) and text[i] == '.' or text[i] == '?'\
+           or text[i] == ':':
+            if flag == 0:
+                temp = text.split(text[i], 1)
+                flag = 1
+            else:
+                temp = substring.split(text[i], 1)
+            new += temp[0].lstrip(' ') + text[i]
+            substring = temp[1]
+            print("{:s}".format(new))
+            print()
+            new = ""
+    if flag == 0:
+        text = text.lstrip(' ')
+        text = text.rstrip(' ')
+        print("{:s}".format(text), end="")
+    else:
+        substring = substring.lstrip(' ')
+        substring = substring.rstrip(' ')
+        print("{:s}".format(substring), end="")

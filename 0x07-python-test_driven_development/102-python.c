ae010131257d6ea287b7242209ecea35b5d49b22
Sanaalagh <Sanaalaghzou@gmail.com>
2024-01-10 16:49:39 -0800

    the 0x07. Python - Test-driven development project "

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,49 +1,18 @@
-#include <Python.h>
-#include <stdio.h>
-#include <string.h>
-#include <float.h>
-#include <sys/types.h>
-#include <python3.4/Python.h>
-#include <python3.4/object.h>
-#include <python3.4/listobject.h>
-#include <python3.4/floatobject.h>
-#include <python3.4/unicodeobject.h>
-#define PY_SSIZE_T_C
 #define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
-/**
- * print_python_string - The implementation for print python bytes function
- * @p: Pointer to the Python string
- * Return: If p is not a valid PyBytesObject, print an error message
- */
+void print_python_string(PyObject *p) {
+    if (!PyUnicode_Check(p)) {
+        fprintf(stderr, "[.] string object info\n");
+        fprintf(stderr, "  [ERROR] Invalid String Object\n");
+        return;
+    }
 
-void print_python_string(PyObject *p)
-{
-	Py_ssize_t str_len;
-	Py_UNICODE *str = NULL;
-	char *fmt_str = "  value: %ls\n";
+    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
+    const char *value = PyUnicode_AsUTF8(p);
 
-	fflush(stdout);
-	printf("[.] string object info\n");
-	fflush(stdout);
-	if ((p != NULL) && (p->ob_type != NULL)
-		&& ((p->ob_type)->tp_name != NULL)
-		&& (strcmp((p->ob_type)->tp_name, "str") == 0))
-	{
-		str = PyUnicode_AsWideCharString(p, &str_len);
-		printf("  type: %s%s\n",
-			   ((PyASCIIObject *)p)->state.compact ? "compact " : "",
-			   ((PyASCIIObject *)p)->state.ascii ? "ascii" : "unicode object");
-		fflush(stdout);
-		printf("  length: %d\n", (int)str_len);
-		fflush(stdout);
-		printf(fmt_str, str);
-		fflush(stdout);
-		PyMem_Free(str);
-	}
-	else
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		fflush(stdout);
-	}
+    fprintf(stderr, "[.] string object info\n");
+    fprintf(stderr, "  type: %s\n", PyUnicode_IS_COMPACT_ASCII(p) ? "compact ascii" : "compact unicode object");
+    fprintf(stderr, "  length: %zd\n", length);
+    fprintf(stderr, "  value: %s\n", value);
 }

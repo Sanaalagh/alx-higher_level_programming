bfa12022a3e1c24ce994022576a1120dff6240ad
Sanaalagh <Sanaalaghzou@gmail.com>
2024-02-14 11:07:36 -0800

    The 0x0E. SQL - More queries  project

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- List genres and the number of shows linked to each in a specific country
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;

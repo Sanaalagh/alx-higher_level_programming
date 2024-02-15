67141d04b5b2b14723f721be3cb71744f5463a36
Sanaalagh <Sanaalaghzou@gmail.com>
2024-02-15 03:43:56 -0800

    Some changes

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,10 @@
 -- List all shows without the genre Comedy
-SELECT tv_shows.title
+SELECT title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name != 'Comedy'
-OR tv_genres.name IS NULL
-GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC;
+WHERE id NOT IN (
+    SELECT show_id
+    FROM tv_show_genres
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,6 @@
-SELECT title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY title, tv_show_genres.genre_id;
+-- List all shows without a genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

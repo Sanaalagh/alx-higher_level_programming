7de8b23bf232462b166b90313a8bdf544b194695
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-13 04:37:33 -0700

    The tasks of 0x13-javascript_objects_scopes_closures  project

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	  constructor(w, h) {
-		      if (w <= 0 || h <= 0) return {};
-		      this.width = w;
-		      this.height = h;
-		    }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) return {};
+    this.width = w;
+    this.height = h;
+  }
 };
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	  let count = 0;
-	  list.forEach(element => {
-		      if (element === searchElement) count++;
-		    });
-	  return count;
+  let count = 0;
+  list.forEach(element => {
+    if (element === searchElement) count++;
+  });
+  return count;
 };

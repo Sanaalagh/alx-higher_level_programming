fd9137bc40b9fe4a400aabb52736f0ed9920b4a9
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-17 08:48:13 -0700

    the  python-object_relational_mapping project

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 """
-A script that lists all states with a name starting with N
+A script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa.
 """
+
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[
-        2], db=argv[3], host="localhost", port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-    cur.close()
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Construct SQL query to select states starting with 'N'
+    sql_query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+
+    # Execute the SQL command
+    cursor.execute(sql_query)
+
+    # Fetch all the rows in a list of tuples
+    results = cursor.fetchall()
+
+    # Print results
+    for row in results:
+        print(row)
+
+    # Close the cursor
+    cursor.close()
+
+    # Close the database connection
     db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
 """
-A script that takes an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+A script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 """
+
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[
-        3], host="localhost", port=3306)
-    cur = db.cursor()
-    cur.execute(
-            "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format
-            (argv[4]))
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-    cur.close()
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Construct SQL query to select states based on user input
+    sql_query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
+            sys.argv[4])
+
+    # Execute the SQL command
+    cursor.execute(sql_query)
+
+    # Fetch all the rows in a list of tuples
+    results = cursor.fetchall()
+
+    # Print results
+    for row in results:
+        print(row)
+
+    # Close the cursor
+    cursor.close()
+
+    # Close the database connection
     db.close()

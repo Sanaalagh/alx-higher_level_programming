66acf43ee77ecd51b49f2d54925fd158837a1d7a
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-28 09:06:04 -0700

    the 0x11-python-network_1 project

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     else:
         q = ""
     try:
-        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+        response = requests.post(
+                'http://0.0.0.0:5000/search_user', data={'q': q})
         data = response.json()
         if data:
             print("[{}] {}".format(data.get('id'), data.get('name')))

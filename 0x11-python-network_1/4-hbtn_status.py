a0fe0dbd8bfcc910b24dee7c604da90a6f2f09c7
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-30 08:14:25 -0700

    documenting

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+Fetches a URL using the requests library
+and displays information about the response body.
+"""
+
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type:", type(r.text))
-    print("\t- content:", r.text)
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+Takes a URL as input,
+sends a request to it using the requests library,
+and displays the value of the variable X-Request-Id in the response header
+"""
+
 import requests
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    r = requests.get(url)
-    print(r.headers.get('X-Request-Id'))
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
